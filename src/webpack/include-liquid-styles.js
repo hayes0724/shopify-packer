73480959b44f956d4af4ab25782b19a1f694e8da
hayes <admin@hayesmarketingfirm.com>
2021-04-27 10:38:42 -0500

    fix: escape css filenames in html webpack plugin

--- src/webpack/include-liquid-styles.js
+++ src/webpack/include-liquid-styles.js
@@ -6,8 +6,12 @@ class IncludeLiquidStylesPlugin {
       HtmlWebpackPlugin.getHooks(compilation).beforeAssetTagGeneration.tapAsync(
         'IncludeLiquidStylesPlugin',
         (data, cb) => {
-          // Clean up escaped characters in vendor files
+          // Clean up JS escaped characters in vendor files
           data.assets.js = data.assets.js.map((file) =>
+            file.includes('%40') ? file.replace(/%40/g, '@') : file
+          );
+          // Clean up CSS escaped characters in vendor files
+          data.assets.css = data.assets.css.map((file) =>
             file.includes('%40') ? file.replace(/%40/g, '@') : file
           );
           // Add liquid (s)css files to html webpack plugin
