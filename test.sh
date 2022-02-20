51a55697591aa35aa6a5f67a29bde81d60cdf94a
hayes <admin@hayesmarketingfirm.com>
2022-02-20 14:57:55 -0600

    ci(deps): removed node 10 support and tests

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t packer-node10 -f tests/node10/Dockerfile .
 docker build -t packer-node12 -f tests/node12/Dockerfile .
 docker build -t packer-node14 -f tests/node14/Dockerfile .
 docker build -t packer-node15 -f tests/node15/Dockerfile .
 
-docker run -it --rm -p packer-node10
 docker run -it --rm -p packer-node12
 docker run -it --rm -p packer-node14
 docker run -it --rm -p packer-node15
