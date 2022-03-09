bec0e9ed0361703a682113ca5300b22ed0cebed6
Daniel Beardsley <daniel.beardsley@gmail.com>
2022-03-08 18:59:12 -0700

    fix: ssl:make timing bug (#180)
    createCA() is async and thus doesn't finish till *after* it returns.
    createCert() *needs* the CA file to exist before it runs.
    
    If both files are missing, this command died every time because
    `createCert()` would be called before the CA file was created.
    
    It would then kill the node process before *either* file was created.
    
    Now we use async/await to order them appropriately and we add a test to
    ensure the files get created.

--- cli/commands/ssl.js
+++ cli/commands/ssl.js
@@ -1,10 +1,10 @@
 const {checkSSL, validate} = require('../../src/server/ssl');
 
-module.exports = (args) => {
+module.exports = async (args) => {
   if (args.check) {
     validate();
   }
   if (args.make) {
-    checkSSL();
+    await checkSSL();
   }
 };
